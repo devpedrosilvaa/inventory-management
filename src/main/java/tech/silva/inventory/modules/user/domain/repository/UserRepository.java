ec6f9e1874d707ed487cd1c2ec476a528b35f17a
unknown <aviaolaranja563@gmail.com>
2025-04-22 15:19:29 -0300

    feat: create User class and UserRepository interface

---
+++ src/main/java/tech/silva/inventory/modules/user/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package tech.silva.inventory.modules.user.domain.repository;
+
+import tech.silva.inventory.modules.user.domain.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+    User save(User user);
+    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
+}
