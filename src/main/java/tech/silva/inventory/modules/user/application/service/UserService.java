f0b41b7d581e425553caf32c85a318abd399fcd6
unknown <aviaolaranja563@gmail.com>
2025-04-22 15:21:24 -0300

    feat: create UserService and implement methods

---
+++ src/main/java/tech/silva/inventory/modules/user/application/service/UserService.java
@@ -0,0 +1,23 @@
+package tech.silva.inventory.modules.user.application.service;
+
+
+import tech.silva.inventory.modules.user.domain.model.User;
+import tech.silva.inventory.modules.user.domain.repository.UserRepository;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
