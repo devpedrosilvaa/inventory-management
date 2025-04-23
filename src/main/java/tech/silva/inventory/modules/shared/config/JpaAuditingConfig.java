08c9ad7a714d1367bb9f26671ce03921b847e23b
unknown <aviaolaranja563@gmail.com>
2025-04-23 11:37:13 -0300

    chore: configure Jpa Auditing

---
+++ src/main/java/tech/silva/inventory/modules/shared/config/JpaAuditingConfig.java
@@ -0,0 +1,17 @@
+package tech.silva.inventory.modules.shared.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class JpaAuditingConfig implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated())
+            return Optional.of(authentication.getName());
+        return Optional.empty();
+    }
+}
